c69185bae996f8bb19361593e53f8c7a7e527549
Petr Novák SAUNEO <petr@sauneo.cz>
2024-05-15 14:14:21 +0200

    install phpmailer

--- src/Controller/ExampleController.php
+++ src/Controller/ExampleController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 use Symfony\Component\Routing\Attribute\Route;
 
 class ExampleController extends AbstractController
@@ -11,11 +13,31 @@ class ExampleController extends AbstractController
     #[Route('/example', name: 'app_example')]
     public function index(): Response
     {
-        $exampleText = 'Hello, world!';
+        $mail = new PHPMailer(true);
+
+        try {
+            // Nastavení PHPMailer pro použití funkce mail()
+            $mail->isMail();
+            
+            // Nastavení e-mailu
+            $mail->setFrom('info@sauneo.cz', 'Mailer');
+            $mail->addAddress('petr@sauneo.cz', 'Petr Novák');
+            $mail->isHTML(true);
+            $mail->Subject = 'Here is the subject';
+            $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+            $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+            $mail->send();
+            return new Response('Message has been sent');
+        } catch (Exception $e) {
+            return new Response("Message could not be sent. Mailer Error: {$mail->ErrorInfo}");
+        }
+
+        /*$exampleText = 'Hello, world!';
 
         return $this->render('example/index.html.twig', [
             'controller_name' => 'ExampleController',
             'exampleText' => $exampleText,
-        ]);
+        ]);*/
     }
 }
