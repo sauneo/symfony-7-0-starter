0e1e02885789fbdb71cdf8115716e473da8bf233
Petr Novák SAUNEO <petr@sauneo.cz>
2024-05-16 11:55:25 +0200

    add Admin dir

--- src/Controller/Admin/ExampleController.php
+++ src/Controller/Admin/ExampleController.php
@@ -1,19 +1,20 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\Admin;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/admin/example')]
 class ExampleController extends AbstractController
 {
-    #[Route('/example', name: 'app_example')]
+    #[Route('/', name: 'app_admin_example')]
     public function index(): Response
     {
-        $exampleText = 'Hello, world!';
+        $exampleText = 'Hello, admin!';
 
-        return $this->render('web/example/index.html.twig', [
+        return $this->render('admin/example/index.html.twig', [
             'controller_name' => 'ExampleController',
             'exampleText' => $exampleText,
         ]);
